7db89adb229d8105210e5e29de0571f368a55ed5
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-12-02 20:09:14 +0100

    day2 refactorio

--- Day02/part1.js
+++ Day02/part1.js
@@ -11,37 +11,23 @@ for (let row of file) {
 
     if (direction === 0 || Math.abs(direction) > 3) continue;
 
-    if (direction < 0) {
-        let flag = false;
-        for (let i = 1; i < row.length - 1; i++) {
-            if (row[i] >= row[i + 1]) {
-                flag = true;
-                break;
-            }
+    const isAscending = direction < 0;
 
-            if (Math.abs(row[i] - row[i + 1]) > 3) {
-                flag = true;
-                break;
-            }
-        }
-        if (flag) continue;
-        safeReports++;
-    } else {
-        let flag = false;
-        for (let i = 1; i < row.length - 1; i++) {
-            if (row[i] <= row[i + 1]) {
-                flag = true;
-                break;
-            }
+    let flagged = false;
 
-            if (Math.abs(row[i] - row[i + 1]) > 3) {
-                flag = true;
-                break;
-            }
+    for (let i = 0; i < row.length - 1; i++) {
+        if (
+            (isAscending && row[i] >= row[i + 1]) ||
+            (!isAscending && row[i] <= row[i + 1]) ||
+            Math.abs(row[i] - row[i + 1]) > 3
+        ) {
+            flagged = true;
+            break;
         }
-        if (flag) continue;
-        safeReports++;
     }
+
+    if (flagged) continue;
+    safeReports++;
 }
 
 console.log(safeReports);
--- Day02/part2.js
+++ Day02/part2.js
@@ -1,5 +1,7 @@
-const readFile = require("fs").readFileSync;
-let file = readFile(__dirname + "/input.txt", "utf-8")
+const fs = require("fs");
+
+const file = fs
+    .readFileSync(__dirname + "/input.txt", "utf-8")
     .replace(/\r/g, "")
     .split("\n")
     .map((row) => row.split(" ").map(Number));
@@ -13,58 +15,43 @@ function checkRow(row) {
         return 0;
     }
 
-    if (direction < 0) {
-        for (let i = 1; i < row.length - 1; i++) {
-            if (row[i] >= row[i + 1]) {
-                return i;
-            }
+    const isAscending = direction < 0;
 
-            if (Math.abs(row[i] - row[i + 1]) > 3) {
-                return i;
-            }
-        }
-    } else {
-        for (let i = 1; i < row.length - 1; i++) {
-            if (row[i] <= row[i + 1]) {
-                return i;
-            }
-
-            if (Math.abs(row[i] - row[i + 1]) > 3) {
-                return i;
-            }
+    for (let i = 1; i < row.length - 1; i++) {
+        if (
+            (isAscending && row[i] >= row[i + 1]) ||
+            (!isAscending && row[i] <= row[i + 1]) ||
+            Math.abs(row[i] - row[i + 1]) > 3
+        ) {
+            return i;
         }
     }
+
     return -1;
 }
 
-for (let row of file) {
-    let faultyIdx = checkRow(row);
+for (const row of file) {
+    const faultyIdx = checkRow(row);
 
     if (faultyIdx === -1) {
         numOfSafetyReports++;
         continue;
-    } else {
-        const spliceMinusOne =
-            faultyIdx > 0
-                ? [...row.slice(0, faultyIdx - 1), ...row.slice(faultyIdx)]
-                : [...row];
-        const spliceZero = [
-            ...row.slice(0, faultyIdx),
-            ...row.slice(faultyIdx + 1),
-        ];
-        const splicePlusOne =
-            faultyIdx < row.length - 1
-                ? [...row.slice(0, faultyIdx + 1), ...row.slice(faultyIdx + 2)]
-                : [...row];
+    }
 
-        for (let idk of [spliceMinusOne, spliceZero, splicePlusOne]) {
-            let isStillFaulty = checkRow(idk);
-            if (isStillFaulty === -1) {
-                numOfSafetyReports++;
-                break;
-            } else {
-            }
-        }
+    const hydra = [
+        faultyIdx > 0 && [
+            ...row.slice(0, faultyIdx - 1),
+            ...row.slice(faultyIdx),
+        ],
+        [...row.slice(0, faultyIdx), ...row.slice(faultyIdx + 1)],
+        faultyIdx < row.length - 1 && [
+            ...row.slice(0, faultyIdx + 1),
+            ...row.slice(faultyIdx + 2),
+        ],
+    ].filter(Boolean);
+
+    if (hydra.some((head) => checkRow(head) === -1)) {
+        numOfSafetyReports++;
     }
 }
 
