15fdc9e73af947b2c48e0bc848e9d7ede379c775
Tomislav <77341991+mrperrytpx@users.noreply.github.com>
2024-12-10 23:00:57 +0100

    Update day10.go

--- Day10/day10.go
+++ Day10/day10.go
@@ -38,7 +38,7 @@ func Solution(lines [][]int) []int {
 			h := lines[row][col]
 			if h == 0 {
 				trailheads := 0      // how many peaks from a single trail
-				trailheadRating := 0 // how many trails to a single peak from the same start
+				trailheadRating := 0 // how many trails to a single peak from the same trail base
 				queue := [][]int{
 					{row, col, h + 1},
 				}
