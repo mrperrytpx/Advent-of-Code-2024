787ba8b25cb12a9f1e3a06c8e2922981ac0d2e1e
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-12-14 15:26:04 +0100

    aoc 2024 d14p2 refactorio, can be 200ms faster if we don't want to see the tree

--- Day14/part2.js
+++ Day14/part2.js
@@ -9,33 +9,53 @@ const ROWS = 103;
 const COLS = 101;
 
 let step = 1;
-while (true) {
+outerLoop: while (true) {
     let tree = Array.from({ length: ROWS }, () => Array(COLS).fill("."));
     let robotsSet = new Set();
     for (let line of file) {
         let [pCol, pRow, vCol, vRow] = line.match(REGEX).map(Number);
-        const newRobotPos = [
+        const [nx, ny] = [
             (((vRow * step + pRow) % ROWS) + ROWS) % ROWS,
             (((vCol * step + pCol) % COLS) + COLS) % COLS,
         ];
 
-        tree[newRobotPos[0]][newRobotPos[1]] = "#";
+        tree[nx][ny] = "#";
 
-        const key = `${[newRobotPos[0], newRobotPos[1]]}`;
-        if (robotsSet.has(key)) continue;
+        const key = `${[nx, ny]}`;
+        if (robotsSet.has(key)) {
+            step++;
+            continue outerLoop;
+        }
         robotsSet.add(key);
     }
 
     if (robotsSet.size === file.length) {
-        for (let line of tree) {
-            fs.appendFileSync(
-                __dirname + `/steps/output-${step}.txt`,
-                line + "\n",
-                "utf8"
-            );
+        for (let robot of robotsSet) {
+            const [x, y] = robot.split(",").map(Number);
+
+            const lkey = `${[x + 1, y - 1]}`;
+            const ckey = `${[x + 1, y]}`;
+            const rkey = `${[x + 1, y + 1]}`;
+
+            // . # .
+            // # # #
+            if (
+                robotsSet.has(lkey) &&
+                robotsSet.has(ckey) &&
+                robotsSet.has(rkey)
+            ) {
+                console.log(step);
+                for (let line of tree) {
+                    fs.appendFileSync(
+                        __dirname + `/output-${step}.txt`,
+                        line + "\n",
+                        "utf8"
+                    );
+                }
+                return;
+            }
         }
     }
 
     step++;
-    if (step === 10000) break;
 }
