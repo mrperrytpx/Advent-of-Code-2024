3144ac51373522949620da011e87ab1c292c2253
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-12-17 14:45:38 +0100

    Advent of Code 2024 day 17 part 1, my head doesn't understand bit operations smile

---
+++ Day17/part1.js
@@ -0,0 +1,44 @@
+const readFile = require("fs").readFileSync;
+let file = readFile(__dirname + "/input.txt", "utf-8")
+    .replace(/\r/g, "")
+    .split("\n\n");
+
+const REGEX = /(\d+)/gm;
+
+let [regA, regB, regC] = file[0].match(REGEX).map(Number);
+const program = file[1].match(REGEX).map(Number);
+console.log(program);
+
+let finalStr = "";
+for (let i = 0; ; i += 2) {
+    if (i > program.length) break;
+    const combo = {
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 3,
+        4: regA,
+        5: regB,
+        6: regC,
+    };
+    const p = program[i];
+    const operand = program[i + 1];
+
+    if (p === 0) regA = Math.trunc(regA / 2 ** combo[operand]);
+    if (p === 1) regB = regB ^ operand;
+    if (p === 2) regB = ((combo[operand] % 8) + 8) % 8;
+    if (p === 3) {
+        if (regA === 0) continue;
+        i = operand;
+        i -= 2;
+    }
+    if (p === 4) regB = regB ^ regC;
+    if (p === 5) {
+        const out = ((combo[operand] % 8) + 8) % 8;
+        finalStr += out.toString();
+    }
+    if (p === 6) regB = Math.trunc(regA / 2 ** combo[operand]);
+    if (p === 7) regC = Math.trunc(regA / 2 ** combo[operand]);
+}
+
+console.log(finalStr.split("").join(""));
