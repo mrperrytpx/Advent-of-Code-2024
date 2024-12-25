bfbb49d7287e20d52ea7f15fcb9d4e97ecfe7e39
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-12-25 12:08:57 +0100

    advent of code day 25 part 1 merry christmas fool

---
+++ Day25/part1.js
@@ -0,0 +1,42 @@
+const readFile = require("fs").readFileSync;
+let file = readFile(__dirname + "/input.txt", "utf-8")
+    .replace(/\r/g, "")
+    .split("\n\n")
+    .map((x) => x.split("\n"));
+
+const H = file[0].length;
+console.log("H", H);
+
+function isMatching(lock, key) {
+    for (let i = 0; i < lock.length; i++) {
+        for (let j = 0; j < lock[i].length; j++) {
+            if (lock[i][j] === "#" && key[i][j] === "#") {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+let locks = [];
+let keys = [];
+let pattern = /^([#])\1*$/;
+
+for (let schematic of file) {
+    if (pattern.test(schematic[0])) {
+        locks.push(schematic);
+    }
+
+    if (pattern.test(schematic[schematic.length - 1])) {
+        keys.push(schematic);
+    }
+}
+
+let sum = 0;
+for (let l of locks) {
+    for (let k of keys) {
+        if (isMatching(l, k)) sum++;
+    }
+}
+
+console.log(sum);
